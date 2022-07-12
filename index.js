420b996eec86f687a7254b473306937a762b1832
fanweixiao <fanweixiao@gmail.com>
2022-07-12 21:56:42 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+
+var a = 1
+
+serve((req) => {
+  const upgrade = req.headers.get("upgrade") || "";
+  if (upgrade.toLowerCase() != "websocket") {
+    return new Response("request isn't trying to upgrade to websocket.");
+  }
+  const { socket, response } = Deno.upgradeWebSocket(req);
+  socket.onopen = () => console.log("socket opened");
+  socket.onmessage = (e) => {
+    console.log("socket message:", Deno.inspect(e.data));
+    socket.send("***" + ++a + "*** " + new Date().toString());
+  };
+  socket.onerror = (e) => console.log("socket errored:", e.message);
+  socket.onclose = () => console.log("socket closed");
+  return response;
+});
+
+console.log("Listening on http://localhost:8000");
